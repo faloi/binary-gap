f17399e929eb0d368f58a922105b8990833efbe7
Federico Aloi <federico.aloi@gmail.com>
2019-07-12 16:28:56 -0300

    Conversion to binary

--- lib/binary_gap.rb
+++ lib/binary_gap.rb
@@ -1,5 +1,13 @@
 module Number
   def self.binary_gap(n)
-    n
+    gaps to_bin(n)
+  end
+
+  def self.gaps(bin)
+    bin.scan(/0+/)
+  end
+
+  def self.to_bin(n)
+    n.to_s(2)
   end
 end
--- spec/binary-gap_spec.rb
+++ spec/binary-gap_spec.rb
@@ -3,8 +3,25 @@ require_relative '../lib/binary_gap'
 
 
 describe 'binary_gap' do
-  context 'pirulo' do
-    let(:result) { Number.binary_gap(8) }
-    it { expect(result).to eq(8) }
+  let(:result) { Number.binary_gap(number) }
+
+  context 'with one gap' do
+    let(:number) { 9 }
+    it { expect(result).to eq(['00']) }
+  end
+
+  context 'with many gaps' do
+    let(:number) { 529 }
+    it { expect(result).to eq(['0000', '000']) }
+  end
+
+  context 'with gaps and leading one' do
+    let(:number) { 20 }
+    it { expect(result).to eq(['0']) }
+  end
+
+  context 'with no gaps' do
+    let(:number) { 15 }
+    it { expect(result).to eq([]) }
   end
 end
